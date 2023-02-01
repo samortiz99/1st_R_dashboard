c3f8b7176f6aa1dd96358510e4a9acfc76df0574
Samuel Ortiz <samuelortiz706@gmail.com>
2023-01-31 20:43:21 -0500

    ui updated

--- ui.R
+++ ui.R
@@ -5,13 +5,32 @@ ui <- dashboardPage(
   
   #header modifica toda la parte superior del dashboard
   header = dashboardHeader(title = "This Dashboard",#titulo
+                           dropdownMenu(type = "message", #icono header tipo notificación
+                                        messageItem(from = "Samuel", #item de mensaje
+                                                    "Pay house's projet")), #mensaje
                            dropdownMenu(type = "notification",
-                                        messageItem(from = "Samuel",
-                                                    "Pay house's projet")), 
-                           dropdownMenu(type = "message",
-                                        messageItem(from = "Samuel",
-                                                    "Hello there"))
-),
-  sidebar = dashboardSidebar(),
+                                        notificationItem(text = "Sacar la basura")) #item de notif.
+
+  ),
+  sidebar = dashboardSidebar(
+    sidebarSearchForm(
+      textId = "",
+      buttonId = "",
+      label = "Here"
+    ),
+    sidebarMenu(id="sidebarID",
+                menuItem("Item1")  
+    ),
+    menuItem("New Items", id="2nd sidebarID",
+             menuSubItem("1st item"),
+             menuSubItem("2nd item"),
+             menuSubItem("3th item")
+             
+    )
+    
+  ),
+  
+  
+  
   body = dashboardBody()
 )
